a6bf641adb37ddbd2e2dc0349e2486b9a70bf875
iceg <isg5455@naver.com>
2023-11-07 11:30:07 +0900

    회전 문제 해결

--- scripts/fake_cmd_vel_publisher_copy.py
+++ scripts/fake_cmd_vel_publisher_copy.py
@@ -6,7 +6,7 @@ from geometry_msgs.msg import Twist
 def cmd_vel_callback(data):
     # 수신한 메시지에서 angular.z 값을 3배로 증폭
     data.angular.z *= 3.0
-    round(data.angular.z, 2)
+    data.angular.z = round(data.angular.z, 2)
     # 증폭된 메시지를 다시 cmd_vel 토픽으로 게시
     cmd_vel_publisher.publish(data)
 
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     rospy.init_node('cmd_vel_amplifier', anonymous=True)
     
     # /cmd_vel 토픽에서 메시지를 받는 subscriber 생성
-    rospy.Subscriber('cmd_vel', Twist, cmd_vel_callback)
+    rospy.Subscriber('fake_cmd_vel', Twist, cmd_vel_callback)
     
     # /cmd_vel 토픽으로 메시지를 게시하는 publisher 생성
     cmd_vel_publisher = rospy.Publisher('cmd_vel', Twist, queue_size=10)
