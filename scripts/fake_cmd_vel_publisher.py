7329a01b18a08aac3e42e00bad9c7af0d096d77e
iceg <isg5455@naver.com>
2023-11-08 16:17:46 +0900

    회전문제 완료

--- scripts/fake_cmd_vel_publisher.py
+++ scripts/fake_cmd_vel_publisher.py
@@ -1,58 +1,52 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 import rospy
-from geometry_msgs.msg import Twist, Vector3 
+from geometry_msgs.msg import Twist
 
-rospy.init_node('cmd_vel_publisher')
+def cmd_vel_callback(data):
+    # 수신한 메시지에서 angular.z 값을 n배로 증폭
+    data.angular.z *= 2.0
+    data.angular.z = round(data.angular.z, 2)
 
-cmd_vel_pub = rospy.Publisher("cmd_vel", Twist, queue_size=30) 
+    # if abs(data.angular.z) == 1.5:
+    #     data.angular.z *= 1.0
+    #     data.angular.z = round(data.angular.z, 2)
+    # else: 
+    #     data.angular.z *= 2.0
+    #     data.angular.z = round(data.angular.z, 2)
+
+    # 증폭된 메시지를 다시 cmd_vel 토픽으로 게시
+    cmd_vel_publisher.publish(data)
+
+if __name__ == '__main__':
+    rospy.init_node('cmd_vel_amplifier', anonymous=True)
+    
+    # /cmd_vel 토픽에서 메시지를 받는 subscriber 생성
+    rospy.Subscriber('fake_cmd_vel', Twist, cmd_vel_callback)
+    
+    # /cmd_vel 토픽으로 메시지를 게시하는 publisher 생성
+    cmd_vel_publisher = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+
+    # 노드를 실행
+    rospy.spin()
 
 
-def cmd_vel_callback(data): #이동값을 cmd_vel에서 받아오는 함수
-    global vx, vy, vz, angular_x, angular_y, angular_z
-    vx = data.linear.x #이동값
-    vy = data.linear.y
-    vz = data.linear.z
 
-    angular_x = data.angular.x #회전값
-    angular_y = data.angular.y #회전값
-    angular_z = data.angular.z #회전값
+# if 0.0 < abs(data.angular.z) < 0.5: 
+#         data.angular.z *= 1.0
+#         data.angular.z = round(data.angular.z, 2)
 
+#     elif 0.5 <= abs(data.angular.z) < 1.0: 
+#         data.angular.z *= 3.0
+#         data.angular.z = round(data.angular.z, 2)
 
-cmd_vel_sub = rospy.Subscriber("fake_cmd_vel", Twist, cmd_vel_callback) #cmd_vel값을 subscriber로 받아오는 구문
-vx = 0.0  
-vy = 0.0
-vz = 0.0
+#     elif 1.0 <= abs(data.angular.z) <= 1.5: 
+#         data.angular.z *= 2.0
+#         data.angular.z = round(data.angular.z, 2)
 
-angular_x = 0.0
-angular_y = 0.0
-angular_z = 0.0
+#     else: 
+#         data.angular.z *= 1.0
+#         data.angular.z = round(data.angular.z, 2)
 
-current_time = rospy.Time.now()
-last_time = rospy.Time.now()
-
-# r = rospy.Rate(144.0)
-while not rospy.is_shutdown():
-    current_time = rospy.Time.now()
-
-    # since all odometry is 6DOF, create a quaternion from yaw
-    # publish the transform over tf
-
-    # publish the message
-    cmd_vel = Twist()
-    if vx == 0.0 and angular_z == 0.0:
-        # If no message received, set cmd_vel to zero
-        cmd_vel.linear = Vector3(0, 0, 0)
-        cmd_vel.angular = Vector3(0, 0, 0)
-    else:
-    # set the velocity
-        # angular_z = 2.0 * angular_z
-        cmd_vel.linear = Vector3(vx, vy, vz)
-        cmd_vel.angular = Vector3(angular_x, angular_y, angular_z)
-
-    # publish the message
-    cmd_vel_pub.publish(cmd_vel)
-
-    last_time = current_time
-    # r.sleep()
+#     # 증폭된 메시지를 다시 cmd_vel 토픽으로 게시
+#     cmd_vel_publisher.publish(data)
